d90bf0ab1d6f7cbabd6cce6df1ba1c23ad5059da
Gonzalo Quiroz <kiyegoo@gmail.com>
2018-10-26 15:07:08 -0500

    Se finaliza la edicion de los campos, faltan corregir detalles

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+let fecha = document.getElementsByClassName("fecha");
+let monto = document.getElementsByClassName("monto");
+let beneficiario = document.getElementsByClassName("beneficiario");
+let monto_letra = document.getElementsByClassName("monto_letra");
+
+var descargar = document.getElementById("descargar");
+var campo = document.getElementById("file");
+
+/* Rotar */
+var e = document.getElementsByClassName("rotar");
+let rotate = 90;
+
+function toggle_visibility() {
+  for (let i = 0; i < e.length; i++) {
+    let top = e[i].style.top;
+    let left = e[i].style.left;
+    if (e[i].style.transform === "rotate(90deg)") {
+      e[i].style.transform = "none";
+      rotate = 0;
+    } else {
+      e[i].style.transform = "rotate(90deg)";
+      e[i].style.top = left;
+      e[i].style.left = top;
+
+      /*if (e[i].className == "fecha rotar") {
+        e[i].style.top =
+          parseFloat(e[i].style.top.replace(/cm/, "")) + 1 + "cm";
+        //e[i].style.top += "10cm";
+      }*/
+      rotate = 90;
+    }
+  }
+}
+
 /*Identificacion del elemento */
 let tag;
 function elemento(e) {
@@ -25,45 +59,16 @@ function elemento(e) {
   //console.log(tag);
 }
 
-/* Rotar */
-var e = document.getElementsByClassName("rotar");
-let rotate = 90;
-
-function toggle_visibility() {
-  for (let i = 0; i < e.length; i++) {
-    let top = e[i].style.top;
-    let left = e[i].style.left;
-    if (e[i].style.transform === "rotate(90deg)") {
-      e[i].style.transform = "none";
-      rotate = 0;
-    } else {
-      e[i].style.transform = "rotate(90deg)";
-
-      /*if (e[i].className == "fecha rotar") {
-        e[i].style.top = left += "1cm";
-        e[i].style.left = top;
-        console.log(e[i].style.left);
-      } else if (e[i].className == "monto_letra rotar") {
-        e[i].style.top = left -= "1cm";
-        e[i].style.left = top;
-      }*/
-
-      e[i].style.top = left;
-      e[i].style.left = top;
-      rotate = 90;
-    }
-  }
-}
-
-let fecha = document.getElementsByClassName("fecha");
-let monto = document.getElementsByClassName("monto");
-let beneficiario = document.getElementsByClassName("beneficiario");
-let monto_letra = document.getElementsByClassName("monto_letra");
-
-var descargar = document.getElementById("descargar");
-var campo = document.getElementById("file");
-
 function mensaje() {
+  let montoLetra_left = monto_letra[0].style.left;
+  let beneficiario_left = beneficiario[0].style.left;
+  if (e[0].style.transform === "rotate(90deg)") {
+    beneficiario_left =
+      parseFloat(beneficiario[0].style.left.replace(/cm/, "")) + 2 + "cm";
+    montoLetra_left =
+      parseFloat(monto_letra[0].style.left.replace(/cm/, "")) + 5 + "cm";
+  }
+
   let css = `.rotar {
     border: 0px solid #dfdfdf;
     position: absolute;
@@ -102,7 +107,7 @@ function mensaje() {
 
   .beneficiario {
     top: ${beneficiario[0].style.top};
-    left: ${beneficiario[0].style.left};
+    left: ${beneficiario_left};
     width: 8cm;
     height: 0.5cm;
     text-align: left;
@@ -110,7 +115,7 @@ function mensaje() {
   
   .monto_letra {
     top: ${monto_letra[0].style.top};
-    left: ${monto_letra[0].style.left};
+    left: ${montoLetra_left};
     width: 13cm;
     height: 0.5cm;
     text-align: left;
